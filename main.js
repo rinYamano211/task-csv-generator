89d1594b5d715a48e9f1a6a1d0073686023ea358
Rin <gtrinrin@gmail.com>
2026-01-29 20:31:32 +0900

    Add hierarchical task structure by process

--- main.js
+++ main.js
@@ -1,1 +1,67 @@
-console.log("CSV Task Generater loaded");
+const tasks = {
+    design: [],
+    implementation: [],
+    test: []
+};
+
+//ボタンを取得
+const generateBtn = document.getElementById("generateBtn");
+
+//ボタンクリック時処理
+generateBtn.addEventListener("click", () => {
+    //プロジェクトのチェックボックスをすべて取得
+    const projectCheckboxes = document.querySelectorAll(
+        'input[name="project"]:checked'
+    );
+
+    //タスク種別のチェックボックスをすべて取得
+    const taskTypeCheckboxes = document.querySelectorAll(
+        'input[name="taskType"]:checked'
+    );
+
+    //取得した値を配列に変換
+    const selectedProjects = Array.from(projectCheckboxes).map(
+        (checkbox) => checkbox.value
+    );
+
+    const selectedTaskTypes = Array.from(taskTypeCheckboxes).map(
+        (checkbox) => checkbox.value
+    );
+
+    //確認用
+    console.log("projects:", selectedProjects);
+    console.log("Task Types:", selectedTaskTypes);
+});
+
+const taskGroups = document.querySelectorAll(".task-group");
+
+taskGroups.forEach((group) => {
+    const addButton = group.querySelector(".add-part-btn");
+    const input = group.querySelector("input");
+    const partsList = group.querySelector("parts-list");
+    const taskKey = group.dataset.task;
+
+    addButton.addEventListener("click", () => {
+        const partName = input.value.trim();
+        if (partName === "") return;
+
+        // データに追加
+        tasks[taskKey].push(partName);
+
+        // 入力欄クリア
+        input.value = "";
+
+        // 再描画
+        renderParts(partsList, task[taskKey]);
+    });
+});
+
+function renderParts(listElement, parts) {
+    listElement.innerHTML = "";
+
+    parts.forEach((part) => {
+        const li = document.createElement("li");
+        li.textContent = part;
+        listElement.appendChild(li);
+    });
+}
